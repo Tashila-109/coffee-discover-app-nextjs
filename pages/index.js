35a8bed164f291d9458fa23e8b7ea3ef10477bb2
Tashila <tashila.dev@gmail.com>
2022-04-12 15:44:43 +0400

    Added in custom document page component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Banner from '../components/Banner';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-
-  const handleOnButtonClick = () => {}
+  const handleOnButtonClick = () => {};
 
   return (
     <div className={styles.container}>
@@ -15,7 +15,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Banner buttonText="View stores nearby" onButtonClick={handleOnButtonClick} />
+        <Banner buttonText='View stores nearby' onButtonClick={handleOnButtonClick} />
+        <div className={styles.heroImage}>
+          <Image src='/static/hero-image.png' alt='Hero-Image' width={700} height={400} />
+        </div>
       </main>
     </div>
   );
