ac338d9dcd8e5b05f55e10fd4b50944f8b4b9f23
Tashila <tashila.dev@gmail.com>
2022-05-05 00:09:07 +0400

    Refactored coffee store

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,8 +1,49 @@
-import { useRouter } from "next/router";
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const CoffeeStore = () => {
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+const CoffeeStore = props => {
   const router = useRouter();
-  return <div>coffee-store</div>;
+
+  const { address, name, neighbourhood } = props;
+
+  return (
+    <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <Link href='/'>
+        <a>Backl to home</a>
+      </Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
+    </div>
+  );
 };
+
+export function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(coffeeStore => coffeeStore.id.toString() === params.id),
+    }, // will be passed to the page component as props
+  };
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map(coffeeStore => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default CoffeeStore;
