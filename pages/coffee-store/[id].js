549f86977f252f5150edd9930c5f4ba225c94585
Tashila <tashila.dev@gmail.com>
2022-08-20 21:54:26 +0400

    Code refactor

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 import classNames from 'classnames';
 
 import { fetchCoffeeStores } from '../../lib/coffee-stores';
-import { StoreContext } from '../_app';
+import { StoreContext } from "../../store/store-context";
 import { isEmpty } from '../../utils';
 
 import styles from '../../styles/coffee-store.module.css';
