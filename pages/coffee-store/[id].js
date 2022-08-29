0263770c93fec1cc63d68292e8bce354ca5be747
Tashila <tashila.dev@gmail.com>
2022-08-29 15:27:18 +0400

    Added in upvote handler

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -64,8 +64,12 @@ const CoffeeStore = initialProps => {
 
   const { name, address, neighbourhood, imgUrl } = coffeeStore;
 
-  const hanelUpvoteButton = () => {
-    console.log('Up vote');
+  const [votingCount, setVotingCount] = useState(1);
+
+  const handleUpvoteButton = () => {
+    console.log('handle upvote');
+    let count = votingCount + 1;
+    setVotingCount(count);
   };
 
   if (router.isFallback) {
@@ -106,7 +110,7 @@ const CoffeeStore = initialProps => {
 
           <div className={styles.iconWrapper}>
             <Image src='/static/icons/star.svg' width={24} height={24} alt='Star' />
-            <p className={styles.text}>{1}</p>
+            <p className={styles.text}>{votingCount}</p>
           </div>
 
           <button className={styles.upvoteButton} onClick={hanelUpvoteButton}>
