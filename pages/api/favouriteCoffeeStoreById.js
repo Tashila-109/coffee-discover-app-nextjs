0cf05564d1283bc01026baf0f5dafeb9a75f01b6
Tashila <tashila.dev@gmail.com>
2022-08-29 23:00:33 +0400

    Updated put logic

--- pages/api/favouriteCoffeeStoreById.js
+++ pages/api/favouriteCoffeeStoreById.js
@@ -1,4 +1,4 @@
-import { findRecordByFilter } from '../../lib/airtable';
+import { table, findRecordByFilter, getMinifiedRecords } from '../../lib/airtable';
 
 const favouriteCoffeeStoreById = async (req, res) => {
   if (req.method === 'PUT') {
@@ -9,7 +9,25 @@ const favouriteCoffeeStoreById = async (req, res) => {
         const records = await findRecordByFilter(id);
 
         if (records.length !== 0) {
-          res.json(records);
+          const record = records[0];
+
+          const calculateVoting = parseInt(record.voting) + 1;
+
+          // update a record
+
+          const updateRecord = await table.update([
+            {
+              id: record.recordId,
+              fields: {
+                voting: calculateVoting,
+              },
+            },
+          ]);
+
+          if (updateRecord) {
+            const minifiedRecords = getMinifiedRecords(updateRecord);
+            res.json(minifiedRecords);
+          }
         } else {
           res.json({ message: "Coffee store id doesn't exist", id });
         }
