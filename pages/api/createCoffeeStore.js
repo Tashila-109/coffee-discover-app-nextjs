175d35e3d9b23c51d39868f92bb02c96f5682d57
Tashila <tashila.dev@gmail.com>
2022-08-29 21:37:23 +0400

    Code refactor

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,4 @@
-import { table, getMinifiedRecords } from "../../lib/airtable";
+import { table, getMinifiedRecords, findRecordByFilter } from '../../lib/airtable';
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === 'POST') {
@@ -8,14 +8,9 @@ const createCoffeeStore = async (req, res) => {
 
     try {
       if (id) {
-        const findCoffeeStoreRecords = await table
-          .select({
-            filterByFormula: `id="${id}"`,
-          })
-          .firstPage();
+        const filteredRecords = await findRecordByFilter(id);
 
-        if (findCoffeeStoreRecords.length !== 0) {
-          const records = getMinifiedRecords(findCoffeeStoreRecords);
+        if (filteredRecords.length !== 0) {
           res.json(records);
         } else {
           //create a record
