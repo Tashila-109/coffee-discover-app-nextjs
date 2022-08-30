d2e336fc692a1393e2a19751a1a8e0f3d9006d32
Tashila <tashila.dev@gmail.com>
2022-08-30 13:04:34 +0400

    Updated up vote handler logic

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -71,16 +71,32 @@ const CoffeeStore = initialProps => {
 
   useEffect(() => {
     if (data && data.length > 0) {
-      console.log('data from SWR', data);
       setCoffeeStore(data[0]);
       setVotingCount(data[0].voting);
     }
   }, [data]);
 
-  const handleUpvoteButton = () => {
-    console.log('handle upvote');
-    let count = votingCount + 1;
-    setVotingCount(count);
+  const handleUpvoteButton = async () => {
+    try {
+      const response = await fetch('/api/favouriteCoffeeStoreById', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
+
+      const dbCoffeeStore = await response.json();
+
+      if (dbCoffeeStore && dbCoffeeStore.length > 0) {
+        let count = votingCount + 1;
+        setVotingCount(count);
+      }
+    } catch (err) {
+      console.error('Error upvoting the coffee store', err);
+    }
   };
 
   if (error) {
